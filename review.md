38d5e93f8a1d9f314de74f6c2be1803b61181f3b
mjlee0326 <mjlee032667@gmail.com>
2021-01-29 08:24:41 +0900

    .

---
+++ review.md
@@ -0,0 +1,35 @@
+정적 컨텐츠: 서버에서 하는 일 없이 바로 웹 브라우저에 전달
+*MVC와 템플릿 엔진*
+템플릿엔진(jsp php): 서버에서 프로그래밍해서 동적으로 (렌더링된 html을) 웹 브라우저에 전달
+MVC: Model, View, Controller
+
+API: json 데이터 포맷으로 클라이언트에게 데이터 전달
+
+
+*정적 컨텐츠*
+
+내장 톰캣 서버에서 요청을 받고 controller에서 먼저 hello-static.html이 있는지 확인(우선 순위를 가짐)
+
+없으면 resources에 있는 static/hello-static.html을 찾아서 return
+
+viewResolver: 화면과 관련됨. 뷰를 찾고 템플릿 연결
+
+템플릿엔진이 렌더링을 해서 변환된 html을 웹 브라우저에 전달
+
+
+
+@ResponseBody
+default: 객체가 오면 json방식으로 데이터를 만들어서, http 응답에 반환
+
+JsonConverter StringConverter
+
+객체를 json으로 바꿔주는 라이브러리 중 대표적: Jackson
+
+
+
+
+
+컨트롤러: 웹 MVC의 컨트롤러 역할
+서비스: 핵심 비즈니스 로직 구현
+리포지토리: 데이터베이스에 접근, 도메인 객체를 DB에 저장하고 관리
+도메인: 비즈니스 도메인 객체 (eg. 회원 주문, 쿠폰 등 주로 데이터베이스에 저장하고 관리됨)
--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,4 +1,38 @@
 package hello.hellospring.repository;
 
-public class MemoryMemberRepository {
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; //sequence는 0,1,2등 키 값을 설정
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); //system id 설정
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        //null일 경우, optional로 감싸서 반환 -> 클라이언트에서 처리
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        //루프를 돌리면서, member.getName이 param으로 넘겨온 name이랑 같은지 확인
+        //하나라도 찾으면 반환(findAny)
+        return store.values().stream()
+                .filter(member->member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); //member들 반환
+    }
 }
